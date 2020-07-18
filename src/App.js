1058b954c04144d431593cbb10477fe38fac36e6
Rhavi Saputra <rhavi.saputra@gmail.com>
2020-07-18 17:28:04 +0700

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './assets/css/App.css';
+
+import Main from './pages/main';
+
+function App() {
+	return (
+		<div className="App">
+			<Main />
+		</div>
+	);
+}
+
+export default App;
